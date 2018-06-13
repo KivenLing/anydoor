cce0e53d74672395481286e795bdd440461a2eda
KivenLing <KivenL96@163.com>
2018-06-13 16:20:39 +0800

    change code style conf

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,19 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
+    "extends": ["eslint:recommended"],
     "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
+      "no-console": ["error", {
+        "allow": ["warn", "error", "info"]
+      }]
+    },
+    "parser": "babel-eslint",
+    "parserOptions": {
+      "ecmaVersion": 6,
+      "sourceType": "script"
+    },
+    "globals": {
+    },
+    "env": {
+      "node": true,
+      "es6": true
     }
-};
+  };
