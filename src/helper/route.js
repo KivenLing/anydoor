3e57dbb96d487bf286c5c8ce51bdd8e9606f2c2f
KivenLing <KivenL96@163.com>
2018-06-13 20:51:02 +0800

    添加样式

--- src/helper/route.js
+++ src/helper/route.js
@@ -2,20 +2,34 @@ const promisify = require('util').promisify;
 const fs = require('fs');
 const stat = promisify(fs.stat);
 const readDir = promisify(fs.readdir);
+const path = require('path');
+const Handlebars = require('handlebars');
+const conf = require('../config/defaultConfig');
+//除require外，尽量用绝对路径，因为许多相对路径指执行命令的路径
+const tplPath = path.join(__dirname, '../template/dir.tpl');
+//这里用同步，原因返回的模板在node启动时加载
+const source = fs.readFileSync(tplPath);
+const template = Handlebars.compile(source.toString());
 module.exports = async function (req, res, filePath) {
     try {
         const stats = await stat(filePath);
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
         if (stats.isFile()){
+            res.setHeader('Content-Type', 'text/plain');
             fs.createReadStream(filePath).pipe(res);
         }else if (stats.isDirectory()) {
+            res.setHeader('Content-Type', 'text/html');
             const files = await readDir(filePath);
-            res.end(files.join(','));
+            const dir = path.relative(conf.root, filePath);
+            const data = {
+                title: path.basename(filePath),
+                files: files,
+                dir: dir ?  `/${dir}` : ''
+            }
+            res.end(template(data));
         }
     } catch (error) {
         res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end(`${filePath} is not found`);
+        res.end(`${filePath} is not found  ${error}`);
     }
 }
